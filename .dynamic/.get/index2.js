70935e6b35e480bd0b21d464c0ac7985739cc1d6
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-12-27 19:56:54 +0000

    Mail forwarding works

--- .dynamic/.get/index2.js
+++ .dynamic/.get/index2.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
+const path = require('path')
 
-const template = fs.readFileSync('.dynamic/index-template.html')
+const template = fs.readFileSync(path.join(__dirname, '..', 'index-template.html'))
 
 module.exports = (request, response) => {
   response.html(template)
