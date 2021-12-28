4f38c9e5502b27de39919e9e11ddb5deb460a37f
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-12-28 16:09:15 +0000

    Render signatories into the list

--- .dynamic/.get/index2.js
+++ .dynamic/.get/index2.js
@@ -1,8 +1,20 @@
 const fs = require('fs')
 const path = require('path')
 
-const template = fs.readFileSync(path.join(__dirname, '..', 'index-template.html'))
+const template = fs.readFileSync(path.join(__dirname, '..', 'index-template.html'), 'utf-8')
 
 module.exports = (request, response) => {
-  response.html(template)
+  let page
+  if (db.confirmedSignatories.length === 0) {
+    page = template.replace("<ul id='signatories'></ul>", "<ul id='signatories'><li>No one yet, why not be the first?</li></ul>")
+  } else {
+    const signatoryListItems = db.confirmedSignatories.reduce((listItems, signatory) => {
+      listItems += `\n<li><a href='${signatory.link}'>${signatory.signatory}</a></li>`
+    }, '')
+
+    const signatoryList = `<ul id='signatories'>${signatoryListItems}</ul>`
+    page = template.replace("<ul id='signatories'></ul>", signatoryList)
+  }
+
+  response.html(page)
 }
