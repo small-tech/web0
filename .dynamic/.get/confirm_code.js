b8554118368ae5613d79a784d7f4f203eb9dc827
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-12-29 14:35:01 +0000

    Make link to stylesheet use absolute path

--- .dynamic/.get/confirm_code.js
+++ .dynamic/.get/confirm_code.js
@@ -5,7 +5,7 @@ template = `<!DOCTYPE html>
   <meta http-equiv='X-UA-Compatible' content='IE=edge'>
   <meta name='viewport' content='width=<device-width>, initial-scale=1.0'>
   <title>web0 manifesto: Thank you for signing!</title>
-  <link rel='stylesheet' href='styles.css'>
+  <link rel='stylesheet' href='/styles.css'>
 </head>
 <body>
   <section id='manifesto'>
--- .dynamic/.get/error.js
+++ .dynamic/.get/error.js
@@ -5,7 +5,7 @@ const errorTemplate = `<!DOCTYPE html>
   <meta http-equiv='X-UA-Compatible' content='IE=edge'>
   <meta name='viewport' content='width=<device-width>, initial-scale=1.0'>
   <title>web0 manifesto error</title>
-  <link rel='stylesheet' href='styles.css'>
+  <link rel='stylesheet' href='/styles.css'>
 </head>
 <body>
   <section id='manifesto'>
