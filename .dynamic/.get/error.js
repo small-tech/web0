5fa59c5336ac869a750f862ad8015305e324e6e2
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-12-29 20:45:34 +0000

    Style “web0” properly in error messages

--- .dynamic/.get/error.js
+++ .dynamic/.get/error.js
@@ -23,11 +23,12 @@ const template = `<!DOCTYPE html>
 module.exports = (request, response) => {
   console.log(request.query.message)
   const errorMessage = request.query.message
-    .replace(/</g, '&lt;')           // No HTML, please, we’re British!
-    .replace(/>/g, '&gt;')           // (While we’re all for vaccinations,
-                                     // this is not the kind of injection we enjoy.) ;)
-    .replace(/&lt;p&gt;/g, "<p>")    // OK, ok, paragraph tags are allowed…
-    .replace(/&lt;\/p&gt;/g, "</p>") // Happy now?
+    .replace(/</g, '&lt;')                              // No HTML, please, we’re British!
+    .replace(/>/g, '&gt;')                              // (While we’re all for vaccinations,
+                                                        // this is not the kind of injection we enjoy.) ;)
+    .replace(/&lt;p&gt;/g, "<p>")                       // OK, ok, paragraph tags are allowed…
+    .replace(/&lt;\/p&gt;/g, "</p>")                    // Happy now?
+    .replace(/web0/g, "<span class='web0'>web0</span>") // Branding, innit?
 
     response.html(template.replace('{{error_message}}', errorMessage))
 }
