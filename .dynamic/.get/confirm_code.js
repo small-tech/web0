35523eae8c64dca654fa054b76a665087dc26228
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-12-31 19:29:07 +0000

    Progress #1: Send email on listing confirmation

--- .dynamic/.get/confirm_code.js
+++ .dynamic/.get/confirm_code.js
@@ -1,3 +1,7 @@
+const redirectToError = require('../redirectToError')
+const slugify = str => require('slugify')(str, {lower: true, strict: true})
+const sendMail = require('../sendMail')
+
 const template = `<!DOCTYPE html>
 <html lang='en'>
 <head>
@@ -21,10 +25,7 @@ const template = `<!DOCTYPE html>
 </html>
 `
 
-const redirectToError = require('../redirectToError')
-const slugify = str => require('slugify')(str, {lower: true, strict: true})
-
-module.exports = (request, response) => {
+module.exports = async (request, response) => {
   const code = request.params.code
   console.log(`Asking to confirm signature with code ${code}`)
 
@@ -53,7 +54,36 @@ module.exports = (request, response) => {
 
       // Thank the person for signing.
       const page = template.replace('{{signature_id}}', slugify(signatory.signatory))
-      return response.html(page)
+      response.html(page)
+
+      // Send an email to confirm and provide a link where the person can delete
+      // their signature if they want to in the future. (Doing so will also
+      // delete their data from our database.)
+      const text = `Hello ${signatory.name.split(' ')[0]},
+
+Thank you for signing the web0 manifesto on behalf of ${signatory} (${link}).
+
+View your signature at:
+
+https://web0.small-web.org/#${slugify(signatory.signatory)}
+
+If you ever want to remove your signature from the manifesto in the future, please use the link below (this will remove all of your data including your name and email address from the web0 site’s database):
+
+https://web0.small-web.org/delete/${code}
+
+Thank you.
+
+Computer @ web0.small-web.org
+Sent on behalf of the humans at Small Technology Foundation.
+--
+Want to talk to a human being?
+Just hit reply and I’ll CC in the folks at hello@small-tech.org for you.
+https://small-tech.org`
+      try {
+        const result = await sendMail(signatoryEmail, 'Thank you for signing the web0 manifesto!', text)
+      } catch (error) {
+        console.error(`Could not send thank you email to ${signatoryEmail}`, error)
+      }
     } else {
       redirectToError(response, 'Signatory already confirmed.')
     }
