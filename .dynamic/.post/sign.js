b6bfc7faad05c2b20a1c84527b2a04e9dc63b7d3
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2022-01-03 16:09:10 +0000

    Simplify the initial url validation regexp to avoid false positives

--- .dynamic/.post/sign.js
+++ .dynamic/.post/sign.js
@@ -77,9 +77,9 @@ const footerTemplate = `
 // extended to require a top-level domain.
 const validEmailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/
 
-// Check for URL syntax.
+// Check for URL syntax (very loosely; as a simple first test).
 // Mirrored from the client-side.
-const validUrlRegExp = /^(?:(?:https?|HTTPS?|ftp|FTP):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-zA-Z\u00a1-\uffff0-9]-*)*[a-zA-Z\u00a1-\uffff0-9]+)(?:\.(?:[a-zA-Z\u00a1-\uffff0-9]-*)*[a-zA-Z\u00a1-\uffff0-9]+)*(?:\.(?:[a-zA-Z\u00a1-\uffff]{2,}))\.?)(?::\d{2,})?(?:[/?#]\S*)?$/
+const validUrlRegExp = /^https:\/\/[^ "<>%{}|\\^`]+\.[^ "<>%{}|\\^`]+$/
 
 function htmlEncode (text) {
   return text
